80ea83d9a2a0d05923cd39546812812b8c41f273
zhaocheng.zhai <zzcheng.me@gmail.com>
2020-01-05 15:32:29 +0800

    chapter5

---
+++ leecode/chapter5/lesson3.js
@@ -0,0 +1,16 @@
+export default (arr) => {
+    let arr = arr.sort((a,b) => a - b);
+    let r = [];
+    let odd = 1;
+    let even = 0;
+    arr.forEach(item => {
+        if (item % 2 === 1) {
+            r[odd] = item;
+            odd += 2;
+        } else {
+            r[even] = item;
+            even += 2;
+        }
+    });
+    return r;
+}
