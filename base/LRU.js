d9005e39d0e3e45571743ea3dd74a1340dcb23c9
zhaocheng.zhai <zzcheng.me@gmail.com>
2020-08-11 20:44:39 +0800

    update: deepclone

--- base/LRU.js
+++ base/LRU.js
@@ -53,20 +53,81 @@
 // }
 
 // LFU算法，当空间满时，通过访问次数，淘汰问次数最小的数据。按照频率维度
-class LFU {
 
-}
+var LFUCache = function (capacity) {
+    this.limit = capacity;
+    this.map = {};
+    this.freqMap = {};
+};
 
-let a = new LFU(3);
+LFUCache.prototype.get = function (key) {
+    let r = -1;
+    if (typeof this.map[key] != "undefined") {
+        let o = this.map[key];
+        r = o.value;
+        //更新频率记录
+        this.updateL(key, o);
+    }
+    return r;
+};
+
+LFUCache.prototype.updateL = function (key, obj) {
+    let freq = obj.freq;
+    let arr = this.freqMap[freq];
+    // 删除原频率记录
+    this.freqMap[freq].splice(arr.indexOf(key), 1);
+    // 清理
+    if (this.freqMap[freq].length == 0) delete this.freqMap[freq];
+    // 跟新频率
+    freq = obj.freq = obj.freq + 1;
+    if (!this.freqMap[freq]) this.freqMap[freq] = [];
+    this.freqMap[freq].push(key);
+};
+
+LFUCache.prototype.set = function (key, value) {
+    if (this.limit <= 0) return;
+    if(typeof key=="undefined"||typeof value=="undefined") throw new Error('key or value is undefined');
+    // 存在则直接更新
+    if (typeof this.map[key] == "undefined") {
+        // 获取频率 key
+        // 判断容量是否满
+        if (Object.keys(this.map).length == this.limit) {
+            let fkeys = Object.keys(this.freqMap);
+            let freq = fkeys[0];
+            // 获取key集合
+            let keys = this.freqMap[freq];
+            // 淘汰首位
+            delete this.map[keys.shift()]; 
+            // 清理
+            if (this.freqMap[freq].length == 0) delete this.freqMap[freq];
+        }
+        // 频率记录是否存在
+        if (!this.freqMap[1]) this.freqMap[1] = [];
+        // 插入新值
+        this.freqMap[1].push(key);
+        this.map[key] = {
+            value: value,
+            freq: 1 // 默认的频率
+        };
+    } else {
+        this.map[key].value = value;
+        //更新频率记录
+        this.updateL(key, this.map[key]);
+    }
+};
+
+let a = new LFUCache(3);
 
 a.set(1,1);
 a.set(2,2);
 a.set(3,3);
-console.log('-----a-----', a);
+
 a.get(1);
 a.get(1);
 a.get(2);
 a.get(3);
+a.get(2);
+a.set(4,4);
 console.log('-----a-----', a);
 
 
--- base/deepclone.js
+++ base/deepclone.js
@@ -1,144 +1,72 @@
-function clonebase (target) {
+// 0811 
+// 基础版本
+function deepclone(target) {
     let cloneObj = {};
-    for(let i in target) {
-        cloneObj[i] = target[i];
+    Object.keys(target).forEach((item) => {
+        cloneObj[key] = target[key]
+    });
+    return cloneObj;
+}
+// 1.0版本 考虑数组
+function deepclone1(target) {
+    let isArray = [].isArray.call(target);
+    let cloneObj = isArray ? [] : {};
+    for (const key in target) {
+        cloneObj[key] = target[key]
     }
     return cloneObj;
 }
 
-function cloneDeep(target) {
-    if (typeof target === 'object') {
-        let cloneObj = {};
-        for (let i in target) {
-            cloneObj[i] = cloneDeep(target[i]);
+// 2.0 考虑递归
+function deepclone2(target) {
+    let isArray = [].isArray.call(target);
+    let cloneObj = isArray ? [] : {};
+    for(const key in target) {
+        cloneObj[key] = typeof target[key] === 'object' ? deepclone2(target[key]) : target[key];
+    }
+    return cloneObj;
+}
+
+// 3.0 考虑循环引用
+function deepclone3(target, map = new Map()) {
+    let isArray = Array.isArray(target);
+    let cloneObj = isArray ? [] : {};
+    for (const key in target) {
+        if (map.has(target)) {
+            return map.get(target);
         }
+        map.set(target, cloneObj);
+        cloneObj[key] = typeof target[key] === 'object' ? deepclone2(target[key]) : target[key];
+    }
+    return cloneObj;
+}
+
+// 4.0 考虑性能
+function deepclone4(target, map = new Map()) {
+    if (typeof target === 'object') {
+        let isArray = Array.isArray(target);
+        let cloneObj = isArray ? [] : {};
+        if (map.has(target)) {
+            return map.get(target)
+        }
+        map.set(target, cloneObj);
+        function forEach(array, cb) {
+            let i = -1;
+            while (++i < array.length) {
+                cb(i, array[i]);
+            }
+            return array;
+        }
+        let keys = isArray ? target : Object.keys(target);
+        forEach(keys, (key, value) => {
+            if (keys) {
+                key = value;
+            }
+            cloneObj[key] = deepclone4(target[key], map);
+        });
+
         return cloneObj;
     } else {
         return target;
     }
 }
-
-const target = {
-    field1: 1,
-    field2: undefined,
-    field3: 'ConardLi',
-    field4: {
-        child: 'child',
-        child2: {
-            child2: 'child2'
-        }
-    }
-};
-// console.log(cloneDeep(target));
-
-function cloneArray (target) {
-    if (typeof target === 'object') {
-        let cloneTarget = Array.isArray(target) ? [] : {};
-        for (let key in target) {
-            cloneTarget[key] = cloneArray(target[key]);
-        }
-        return cloneTarget;
-    } else {
-        return target;
-    }
-}
-
-const targetArray = {
-    field1: 1,
-    field2: undefined,
-    field3: {
-        child: 'child'
-    },
-    field4: [2, 4, 8]
-};
-targetArray.targetArray = targetArray;
-
-
-// console.log(cloneDeep(targetArray))
-// console.log(cloneArray(targetArray), 'array');
-
-function cloneMap (target, map = new Map()) {
-    if (typeof target === 'object') {
-        let cloneTarget = Array.isArray(target) ? [] : {};
-        if (map.get(target)) {
-            return target;
-        }
-        map.set(target, cloneTarget);
-        for (let key in target) {
-            cloneTarget[key] = cloneMap(target[key], map);
-        }
-        return cloneTarget;
-    } else {
-        return target;
-    }
-}
-
-// console.log('----------', cloneMap(targetArray));
-
-function cloneWeakMap(target, map = new WeakMap()) {
-    if (typeof target === 'object') {
-        let cloneTarget = Array.isArray(target) ? [] : {};
-        if (map.get(target)) {
-            return target;
-        }
-        map.set(target, cloneTarget);
-        for (let key in target) {
-            cloneTarget[key] = cloneWeakMap(target[key], map)
-        }
-        return cloneTarget;
-    } else {
-        return target;
-    }
-}
-
-function forEach(array, cb) {
-    let idx = 0;
-    let len = array.length;
-    while(idx < len) {
-        cb(array[idx], idx);
-        idx += 1;
-    }
-    return array;
-}
-function cloneWhile(target, map = new WeakMap()) {
-    if (typeof target === 'object') {
-        let isArray = Array.isArray(target);
-        let cloneTarget = isArray ? [] : {};
-        if (map.get(target)) {
-            return target;
-        }
-        map.set(target, cloneTarget);
-        let keys = isArray ? undefined : Object.keys(target);
-        let count = 1;
-        forEach(keys || target, (value, key) => {
-            count ++;
-            console.log(count, keys, value, '----------', key);
-            if (keys) {
-                key = value;
-            }
-            cloneTarget[key] = cloneWhile(target[key], map);
-        });
-        return cloneTarget;
-    } else {
-        return target;
-    }
-}
-
-const targetWhile = {
-    field1: 1,
-    field2: undefined,
-    field3: {
-        child: 'child'
-    },
-    field4: [2, 4, 8],
-    f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: {} } } } } } } } } } } },
-};
-
-targetWhile.targetWhile = targetWhile;
-
-// console.time();
-const result2 = cloneWhile(targetWhile);
-// console.timeEnd();
-
-
-
