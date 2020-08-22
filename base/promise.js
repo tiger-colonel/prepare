d716a688b5ddea47c22817c862d8567a7fedc743
zhaocheng.zhai <zzcheng.me@gmail.com>
2020-08-23 00:26:14 +0800

    review promise && reduce

--- base/promise.js
+++ base/promise.js
@@ -1,228 +1,100 @@
-function resolvePromise(x, promise2, resolve, reject) {
+const all = (promises) => {
+    return new Promise((resolve, reject) => {
+        let arr = [];
+        let len = promises.length;
+        let count = 0;
+        for (let i = 0; i < len; i++) {
+            promises[i].then((res) => {
+                arr[i] = res;
+                count++;
+                count === promises.length && resolve(arr);
+            }, err => {
+                reject(err)
+            })
+        }
+    })
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
     if (x === promise2) {
-        throw new Error('circle ')
+        reject(new Error('sb'))
     }
-    let called;
-    if (typeof promise2 !== null && (typeof promise2 !== 'object' && typeof promise2 === 'function')) {
-        try {
-            let then = x.then;
-            if (typeof then === 'function') {
-                then(value => {
-                    if (called) return ;
-                    called = true;
-                    resolvePromise(value, promise2, resolve, reject)
-                }, err => {
-                    if (called) return ;
-                    called = true;
-                    reject(err)
-                })
-            } else {
-                resolve(x)
-            }
-        } catch (error) {
-            if (called) return ;
-            called = true;
-            reject(console.error(123))
+    if (typeof promise2 !== null && (typeof promise2 === 'function' || typeof promise2 === 'object')) {
+        if (typeof x.then === 'function') {
+            x.then(() => {
+                resolvePromise(promise2, x, resolve, reject)
+            })
+        } else {
+            resolve(x);
         }
     } else {
         resolve(x);
     }
 }
-class MyPromise {
-    constructor(exectutor) {
+class Promise {
+    constructor(exec) {
         this.state = 'pending';
-        this.value = '';
-        this.reason = '';
-        this.resolvedFnCallbacks = [];
-        this.rejectedFnCallbacks = [];
+        this.value = undefined;
+        this.reason = undefined;
+        this.fulfilledCallbacks = [];
+        this.rejectedCallbacks = [];
+
         let resolve = (value) => {
             this.state = 'fulfilled';
             this.value = value;
-            this.resolvedFnCallbacks.forEach(cb => cb())
+            this.fulfilledCallbacks.forEach(fn => fn());
         }
         let reject = (reason) => {
             this.state = 'rejected';
             this.reason = reason;
-            this.rejectedFnCallbacks.forEach(cb => cb())
+            this.rejectedCallbacks.forEach(fn => fn());
         }
         try {
-            exectutor(resolve, reject)
+            exec(resolve, reject)
         } catch (error) {
-            reject(error);
-        }
-    }
-    then(onFulfilled, onRejected) {
-        let promise2 = new Promise((resolve, reject) => {
-            if (this.state === 'pending') {
-                this.resolvedFnCallbacks.push(() => {
-                    setTimeout(() => {
-                        let x = onFulfilled(this.value);
-                        resolvePromise(x, promise2, resolve,reject)
-                    });
-                })
-                this.rejectedFnCallbacks.push(() => {
-                    setTimeout(() => {
-                        let x = onRejected(this.reason);
-                        resolvePromise(x, promise2, resolve, reject)
-                    });
-                })
-            }
-            if (this.state === 'fulfilled') {
-                this.resolvedFnCallbacks.push(() => {
-                    setTimeout(() => {
-                        let x = onFulfilled(this.value);
-                        resolvePromise(x, promise2, resolve,reject)
-                    });
-                })
-            }
-            if (this.state === 'rejected') {
-                this.rejectedFnCallbacks.push(() => {
-                    setTimeout(() => {
-                        let x = onRejected(this.reason);
-                        resolvePromise(x, promise2, resolve, reject)
-                    });
-                })
-            }
-        })
-        return promise2;
-    }
-    all(promises) {
-        let arr = [];
-        return new Promise((resolve, reject) => {
-            for(let i = 0, len = promises.length; i < len; i++) {
-                promises[i].then(data => {
-                    arr.push(data);
-                })
-                if (arr.length === len) {
-                    resolve(arr);
-                }
-            }
-        })
-    }
-    all1(promises) {
-        let arr = [];
-        let i = 0;
-        function processData(index, data, resolve) {
-            arr[i] = data;
-            i++
-            if (i === promises.length) {
-                resolve(arr)
-            }
-        }
-        return new Promise((resolve, reject) => {
-            for (let i = 0; i < promises.length; i++) {
-                promises[i].then(data => {
-                    processData(i, data, resolve)
-                }, reject)
-            }
-        })
-    }
-}
-
-function resolvePromise1(x, promise2, resolve, reject) {
-    if (x === promise2) {
-        reject('circle')
-    }
-    let called;
-    if (typeof != null && (typeof promise2 === 'object' || typeof promise2 === 'function')) {
-        try {
-            let then = x.then;
-            if (typeof then === 'function') {
-                then(value=> {
-                    if (called) return;
-                    called = true;
-                    resolvePromise1(value, promise2, resolve, reject)
-                }, reason => {
-                    if (called) return;
-                    called = true;
-                    reject(reason)
-                })
-            } else {
-                resolve(x);
-            }
-        } catch (error) {
-            if (called) return;
-            called = true;
             reject(error)
         }
         
-    } else {
-        resolve(x)
     }
-}
-
-class MyPromise1 {
-    constructor(exectutor) {
-        this.state = 'pending';
-        this.value = '';
-        this.reason = '';
-        this.onFulfilledCbs = [];
-        this.onRejectedCbs = [];
-        let resolve = (value) => {
-            if (this.state === 'pending') {
-                this.state = 'fulfilled';
-                this.value = value;
-                this.onFulfilledCbs.forEach(fn => fn())
-            }
-        }
-        let reject = (reason) => {
-            if (this.state === 'pending') {
-                this.state = 'rejected';
-                this.reason = reason;
-                this.onRejectedCbs.forEach(fn => fn())
-            }
-        }
-        try {
-            exectutor(resolve, reject)
-        } catch (error) {
-            reject(error)
-        }
-    }
-    then(onFulfilled, onRejected) {
-        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
-        onRejected = typeof onRejected === 'function' ? onRejected : value => value;
-        let promise2 = new Promise((resolve, reject) => {
-            if (this.state === 'pending') {
-                this.onFulfilledCbs.push(() => {
-                    setTimeout(() => {
-                        try {
-                            let x = onFulfilled(this.value);
-                            resolvePromise1(x, promise2, resolve, reject)
-                        } catch (error) {
-                            reject(error)
-                        }
-                    });
-                })
-                this.onRejectedCbs.push(() => {
-                    setTimeout(() => {
-                        try {
-                            let x = onRejected(this.reason);
-                            resolvePromise1(x, promise2, resolve, reject)
-                        } catch (error) {
-                            reject(error)
-                        }
-                    });
-                })
-            }
+    then(onFulfilledFn, onRejectedFn) {
+        let promise2 = new Promise((resolve, reject) =>{
             if (this.state === 'fulfilled') {
-                setTimeout(() => {
-                    try {
-                        let x = onFulfilled(this.value);
-                        resolvePromise1(x, promise2, resolve, reject)
-                    } catch (error) {
-                        reject(error)
-                    }
-                });
+                try {
+                    setTimeout(() => {
+                        let x = onFulfilledFn(this.value);
+                        resolvePromise(promise2, x, resolve, reject);
+                    });
+                } catch (error) {
+                    reject(error)
+                }
             }
             if (this.state === 'rejected') {
-                setTimeout(() => {
-                    try {
-                        let x = onRejected(this.value);
-                        resolvePromise1(x, promise2, resolve, reject)
-                    } catch (error) {
-                        reject(error)
-                    }
-                });
+                try {
+                    setTimeout(() => {
+                        let x = onRejectedFn(this.value);
+                        resolvePromise(promise2, x, resolve, reject)
+                    });
+                } catch (error) {
+                    reject(error)
+                }
+            }
+            if (this.state === 'pending') {
+                try {
+                    this.fulfilledCallbacks.push(() => {
+                        setTimeout(() => {
+                            let x = onFulfilledFn(this.value);
+                            resolvePromise(promise2, x, resolve, reject);
+                        });
+                    })
+                    this.rejectedCallbacks.push(() => {
+                        setTimeout(() => {
+                            let x = onRejectedFn(this.value);
+                            resolvePromise(promise2, x, resolve, reject);
+                        });
+                    })
+                } catch (error) {
+                    reject(error)
+                }
             }
         })
         return promise2;
--- base/reduce.js
+++ base/reduce.js
@@ -1,17 +1,10 @@
-function Difference(arr = [], oarr = []) {
-    return arr.reduce((t, v) => (!oarr.includes(v) && t.push(v), t), []);
-}
-
-// const arr1 = [1, 2, 3, 4, 5];
-// const arr2 = [2, 3, 6];
-// Difference(arr1, arr2); // [1, 4, 5]
 
 // 乘法口诀
-let a = [...new Array(9).keys()].reduce((t, v, i) => t + [...new Array(i + 1).keys()].reduce((u,w,j) => `${u}${i + 1} x ${j + 1} = ${(i + 1) * (j + 1)} `, '').trim() + '\n', '\n');
-// console.log(a);
+let multiple = [...new Array(9).keys()].reduce((t, v, i) => t + [...new Array(i + 1).keys()].reduce((u,w,j) => `${u}${i + 1} x ${j + 1} = ${(i + 1) * (j + 1)} `, '').trim() + '\n', '\n');
+// console.log(multiple);
 
 // ，求数组全部子集, 有两层concat
-let b = [1,2,3,4,5,6].reduce((t, item) => t.concat(t.map(v => v.concat(item))), [[]])
+// let b = [1,2,3,4,5,6].reduce((t, item) => t.concat(t.map(v => v.concat(item))), [[]])
 // console.log(b);
 // function allChildren(arr) {
 //     return arr.reduce((t, item) => {
@@ -48,40 +41,99 @@ function p4(a) {
 
 const promiseArr = [p1, p2, f3, p4];
 
-runPromiseInSequence(promiseArr, 2).then(res => console.log(res, '1'));
+// runPromiseInSequence(promiseArr, 2).then(res => console.log(res, '1'));
 
 
-
-// 手写自己的reduce
-
-function myreduce(cb) {
-    // 拿到调用的数组
-    let arr = this, len = arr.length;
-    // 初始值
-    let k = 0;
-    // 
-    let value;
-    // 取到初始值
-    if (arguments.length > 1) {
-        value = arguments[1];
-    } else {
-        while (k < len && !(k in o)) {
-          k++; 
-        }
-        if (k > len) {
-            // 报错，Reduce of empty array with no initial value
-        }
-        value = this[k++];
-    }
-    // 计算
-    while (k < len) {
-        if (k in this) {
-            value = cb.call(null, value, this[k], k, this)
-        }
-        k++
-    }
-
-    return value;
+// 25个reduce用法
+let a = [1,2,3,4]
+// 1. 求和
+function add(arr) {
+  return arr.reduce((t,v) => (t += v, t));
 }
+// console.log('----------', add(a));
+// 2. reverse
+function reverse(arr) {
+  return arr.reduceRight((t,v) => (t.push(v), t),[])
+}
+// console.log('----------', reverse(a));
+// 3. map
+function map(arr) {
+  return arr.reduce((t, v) => [...t, v * 2], []);
+}
+function filter(arr) {
+  return arr.reduce((t, v) => v > 1 ? [...t, v] : t, [])
+}
+// console.log('-----map(a)-----', filter(a));
+// 数组分割
+function chunk(arr, size) {
+  let result = [];
+  for (let i = 0; i < arr.length; i+=size) {
+    let tmp = [];
+    for (let j = i; j < i + size; j++) {
+      arr[j] && tmp.push(arr[j]);
+    }
+    result.push(tmp);
+  }
+  return result;
+}
+
+function chunkByReduce(arr, size) {
+  return arr.reduce((t, v) => (t[t.length - 1].length === size ? t.push([v]) : t[t.length - 1].push(v), t), [[]])
+}
+
+// console.log('-----result-----', chunkByReduce([1,3,4,5,2,6,7,8,9,10], 3));
+function flat(arr) {
+  let result;
+
+  arr.forEach(item => {
+    result.concat(Array.isArray(item) ? flat(item) : item)
+  })
+}
+function flat1(arr) {
+  return arr.reduce( (t, v) => t.concat(Array.isArray(v) ? flat1(v) : v), []);
+}
+// console.log('----------', flat1([1,[1,2,[4,5,[123], 5,32]]]));
+function uniq(arr) {
+  return arr.reduce((t, v) => t.includes(v) ? t : [...t, v], [])
+}
+// console.log('-----1-----', uniq([1,2,3,1,2,3,3,3,2]));
+function count(arr) {
+  return arr.reduce((t, v) => {
+    t[v] = (t[v] || 0) + 1;
+    return t;
+  }, {})
+}
+// console.log('-----count-----', count([1,2,3,1,2,3,3,3,2]));
+function position(arr, val) {
+  return arr.reduce((t, v, i) => {
+    v === val ? t.push(i): t;
+    return t;
+  }, [])
+}
+// console.log('-----position-----', position([1,2,3,1,2,3,3,3,2], 3));
+function reverseStr(str) {
+  return str.split('').reduceRight((t, v) => t + v, '');
+}
+// console.log('-----reverseStr-----', reverseStr(`let's us study js`));
+
+// 千分位
+function thousandNum(num = 0) {
+  const str = (+num).toString().split('.');
+
+  const integer = nums => nums.split('').reverse().reduceRight((t, v, i) => t + (i % 3 ? v : `${v},`), '').replace(/^,|,$/g, '');
+  const fraction = nums => nums.split('').reduce((t, v, i) => t + ((i + 1) % 3 ? v : `${v},`), '').replace(/^,|,$/g, '');
+  return str.length > 0 ? `${integer(str[0])}.${fraction(str[1])}` : integer(str[0]);
+}
+// console.log('-----cal-----', thousandNum(12345.12345));
+
+// 斐波那锲数列
+function fibonaci(num) {
+  let arr = [...new Array(num).keys()];
+  return arr.reduce((t, v, i) => (i > 1 && t.push(t[i - 1] + t[i - 2]), t), [0, 1])
+}
+console.log('-----cal-----', fibonaci(10));
+
+
+
 
 
