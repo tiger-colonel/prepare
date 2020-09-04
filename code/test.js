5461ee0aeb9cec5ba42540a095448f1180af4c73
zhaocheng.zhai <zzcheng.me@gmail.com>
2020-09-05 00:12:37 +0800

    update

--- code/test.js
+++ code/test.js
@@ -48,7 +48,7 @@ function test(root) {
     return dfs(root);
     // return arr;
 }
-console.log('----------', test(tree));
+// console.log('----------', test(tree));
 
 function solve(root) {
     let stack = [];
@@ -120,5 +120,101 @@ function minDeepByBSF(root) {
     }
     return ans;
 }
+
+function isMirror(root) {
+    const mirror = (left, right) => {
+        if (!left && !right) return true;
+        if (left && right) {
+            return left.val === right.val && mirror(left.left, mirror.right) && mirror(right.left, right.right)
+        }
+        return false;
+    }
+    if (!root) {
+        return true;
+    } else {
+        return mirror(root.left, root.right)
+    }
+}
+function isMirrorBSF(root) {
+    if (!root) return;
+    let qu = [root.left, root.right];
+    while(qu.length) {
+        for (let i = 0; i < qu.length; i+=2) {
+            let ll = qu.shift();
+            let rr = qu.shift();
+            if ((!ll && rr) || (ll && !rr) || (ll.val !== rr.val)) {
+                return false
+            }
+            if (ll && rr) {
+                qu.push(ll.left, rr.right);
+                qu.push(ll.right, rr.left);
+            }
+        }
+    }
+    return true;
+}
+
+function isMirrorByStack(root) {
+    if (!root) return true;
+    let leftStack = [], rightStack = [];
+    let curLeft = root.left;
+    let curRight = root.right;
+    while (curLeft || curRight || leftStack || rightStack) {
+        while (curLeft) {
+            leftStack.push(curLeft);
+            curLeft = curLeft.left;
+        }
+        while (curRight) {
+            leftStack.push(curRight);
+            curRight = curRight.left;
+        }
+        if (leftStack.length !== rightStack.length) return false;
+
+        curLeft = leftStack.pop();
+        curRight = rightStack.pop();
+
+        if (curLeft.val !== curRight.val) return false;
+
+        curLeft = curLeft.right;
+        curRight = curRight.left;
+    }
+    return true;
+}
+
+function reverseChain(head) {
+    let [prev, cur] = [null, head];
+    while(cur) {
+        [cur.next, prev, cur] = [prev, cur, cur.next];
+        // let tmp = cur.next;
+        // cur.next = prev;
+        // prev = cur;
+        // cur = tmp;
+    }
+    return cur;
+}
+function arrayByBSF(root) {
+    if (!root) {
+        return []
+    }
+    let qu = [root];
+    let res = [];
+    let row = 0; // 记录行数
+    while (qu.length) {
+        row++;
+        let arr = [];
+        let tmp = [];
+        for (let i = 0; i < qu.length; i++) {
+            arr.push(qu[i].val);
+            tmp.push(qu[i].left, qu[i].right);
+        }
+        if (row % 2 !== 0) {
+            res.concat(arr.reverse())
+        } else {
+            res.concat(arr);
+        }
+    }
+    return res;
+}
+
 
 
