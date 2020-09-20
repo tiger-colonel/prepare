8a96d0377bafa4817d7526e4e05e0575c82477ce
zhaocheng.zhai <zzcheng.me@gmail.com>
2020-09-21 00:00:46 +0800

    update

--- just_code/0920.js
+++ just_code/0920.js
@@ -1,3 +1,18 @@
 function reverseBetween(head, m, n) {
-    
+    let sentryHead = new ListNode(0);
+    sentryHead.next = head;
+    let tmp = sentryHead;
+    for (let i = 0; i < m - 1; i++) {
+        tmp = tmp.next;
+    }
+    let [prev, cur] = [null, tmp.next]
+    for (let i = 0; i<= n; i++) {
+        let next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    tmp.next.next = cur;
+    tmp.next = prev;
+    return sentryHead.next;
 }
